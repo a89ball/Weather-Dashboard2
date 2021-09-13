d1f5997edb6569907c6accb33e04146e75f598db
Abdifatah <abdifatah.101@outlook.com>
2021-09-13 13:01:54 +0100

    setting stored city

--- script.js
+++ script.js
@@ -5,3 +5,46 @@ $(document).ready(function () {
 
     moment();
 
+    init();
+  
+
+    function storeInput() {
+      userInputs = JSON.parse(localStorage.getItem("userInput")) || [];
+      let set = new Set(userInputs);
+      set.add(cityName);
+      const toArr = Array.from(set);
+      localStorage.setItem("userInput", JSON.stringify(toArr));
+    }
+  
+
+    function renderInputs() {
+      if (localStorage.getItem("userInput")) {
+        $(".history").empty();
+        userInputs = [];
+        const savedInputs = JSON.parse(localStorage.getItem("userInput"));
+        userInputs.push(...savedInputs);
+        userInputs.forEach(function (cityName) {
+          const userInput = $("<li>");
+          userInput.text(cityName);
+          userInput.addClass("list-group-item");
+          userInput.attr("value", cityName);
+          $(".history").append(userInput);
+        });
+      } else {
+        console.log("No stored cities");
+      }
+    }
+  
+
+    function init() {
+      if (localStorage.getItem("userInput")) {
+        const savedInputs = JSON.parse(localStorage.getItem("userInput"));
+        userInputs.push(...savedInputs);
+        console.log(userInputs);
+        cityName = userInputs[userInputs.length - 1];
+      } else {
+        cityName = "London";
+      }
+      apiCall(cityName);
+      renderInputs();
+    }
